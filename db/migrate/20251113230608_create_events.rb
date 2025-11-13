234d8537d4a1361431fb792e6bdc784b4e40613b
John Grundmeyer <jonathan-grundmeyer@uiowa.edu>
2025-11-13 17:07:04 -0600

    add updated event migration

--- db/migrate/20251113230608_create_events.rb
+++ db/migrate/20251113230608_create_events.rb
@@ -2,7 +2,10 @@ class CreateEvents < ActiveRecord::Migration[7.1]
   def change
     create_table :events do |t|
       t.string :name
+      t.string :location
       t.datetime :date
+      t.time :length
+      t.string :theme
 
       t.timestamps
     end
