1d9c4519f1827a50c6c731f6f4998637bc3a877b
John Grundmeyer <jonathan-grundmeyer@uiowa.edu>
2025-11-13 16:38:48 -0600

    event and gift tables/models

---
+++ app/models/gift.rb
@@ -0,0 +1,8 @@
+class Gift < ApplicationRecord
+  belongs_to :user # aka the buyer
+  belongs_to :event
+
+  validates :user, presence: true
+  validates :price, presence: true,  numericality: {greater_than_or_equal_to: 0}
+
+end
