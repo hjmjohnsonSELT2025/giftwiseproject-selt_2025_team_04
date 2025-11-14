ac7a5f5907880dde355fc0414080e578addfda37
tcorme <tyler-orme@uiowa.edu>
2025-11-14 13:47:06 -0600

    test: add test for logging out

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -13,6 +13,15 @@ RSpec.describe "Homes", type: :request do
       get home_index_path
       expect(response).to have_http_status(:ok)
     end
-  end
 
+    it "redirects user to sign in page after logging out" do
+      user = User.create!(email:"user67@bruh.com", password:"password123")
+      sign_in user
+      get home_index_path
+      expect(response).to have_http_status(:ok)
+      sign_out user
+      get home_index_path
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
