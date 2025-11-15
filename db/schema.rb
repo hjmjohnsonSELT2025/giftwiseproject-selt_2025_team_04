48b74ddf9a01de7b69d460df4f1e148eefa6ede7
James Whipple <james.whipple16@gmail.com>
2025-11-14 21:36:38 -0600

    remove gem_rbs_collection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_13_231200) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_14_021532) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
--- features/new_event.feature
+++ features/new_event.feature
@@ -2,7 +2,6 @@ Feature: I can add events to the database
 
   Background: I am logged in
     Given the test user logs in:
-    Then I should see the welcome message
 
   Scenario:
     When I click Add new event
