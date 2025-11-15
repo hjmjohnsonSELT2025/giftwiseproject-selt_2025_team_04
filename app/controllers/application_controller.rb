3cb60754acf627609b13b141e9377874fe15c3b5
John Grundmeyer <jonathan-grundmeyer@uiowa.edu>
2025-11-14 19:36:16 -0600

    fix appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, unless: :testing?
-
-  private
-  def testing?
-    Rails.env.test?
-  end
+  before_action :authenticate_user!
 end
