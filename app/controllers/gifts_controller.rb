bc894a01534aea40d9b2acf514c63bdb7fcf6a21
John Grundmeyer <jonathan-grundmeyer@uiowa.edu>
2025-11-20 20:49:12 -0600

    add more gift functionality

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,4 +1,8 @@
 class GiftsController < ApplicationController
+  def show
+    @gift = current_user.gifts.find(params[:id])
+  end
+
   def new
     @new_gift = Gift.new # for use with the form_with helper
   end
@@ -13,6 +17,29 @@ class GiftsController < ApplicationController
       flash[:alert] = "Could not save gift."
       render :new
     end
+  end
+
+  def edit
+    @gift = current_user.gifts.find(params[:id])
+  end
+
+  def update
+    @gift = current_user.gifts.find(params[:id])
+    @save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => nil, :event_id => nil}
+    if @gift.update(@save_params)
+      flash[:notice] = "Gift updated."
+      redirect_to :root
+    else
+      flash[:alert] = "Could not update gift."
+      render :edit
+    end
+  end
+
+  def destroy
+    @gift = current_user.gifts.find(params[:id])
+    @gift.destroy
+    flash[:notice] = "#{@gift.name} successfully deleted."
+    redirect_to :root
   end
 
   private
