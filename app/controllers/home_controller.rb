114c555155aef0e27bb44b523cf67943dedd903a
John Grundmeyer <jonathan-grundmeyer@uiowa.edu>
2025-11-26 13:39:15 -0600

    follow convention

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @gifts = Gift.all
+    @gifts = current_user.gifts
     @username = current_user.email
     @username = @username[0,@username.index('@')]
     @events = Event.all.order(date: :desc)
