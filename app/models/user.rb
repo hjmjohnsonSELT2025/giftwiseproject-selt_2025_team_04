6d832dbe12981b7059ad88495c4479c6506aedc7
tcorme <tyler-orme@uiowa.edu>
2025-11-27 17:44:27 -0600

    add: create likes, dislikes, age and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipients
+  has_many :likes, dependent: :destroy
+  has_many :dislikes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:github]
+
+  VALID_PRONOUNS = %w[He/Him She/Her They/Them Other]
+  validates :pronouns, inclusion: {in: VALID_PRONOUNS}, allow_nil: true
+  validates :age, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 180}, allow_nil: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -13,4 +19,5 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
     end
   end
+
 end
