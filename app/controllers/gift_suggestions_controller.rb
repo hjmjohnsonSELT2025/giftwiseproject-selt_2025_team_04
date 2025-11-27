60fe1f62318f53ce2c9651c8c2da458a9f20be19
nicwilcox <nicwilcox@uiowa.edu>
2025-11-27 14:41:35 -0600

    Gift suggestions fully working, just need to connect with AI. Views all updated for suggestions. Tweaked other views/controllers to work with suggestions

--- app/controllers/gift_suggestions_controller.rb
+++ app/controllers/gift_suggestions_controller.rb
@@ -1,38 +1,107 @@
 class GiftSuggestionsController < ApplicationController
-  #before_action :authenticate_user! I need to implement OAuth in my stuff still
-  #before_action :set_event still need to connect
+  before_action :authenticate_user!
+  before_action :set_event
+  before_action :set_recipient
 
-  skip_before_action :authenticate_user!, only: [:new, :create], raise: false
 
+  def index
+    if @recipient
+      @suggestions = GiftSuggestion.where(event_id: @event.id, recipient_id: @recipient.id)
+    else
+      @suggestions = GiftSuggestion.where(event_id: @event.id)
+    end
+    #@suggestions = GiftSuggestion.where(event: @event, recipient: @recipient)
+  end
   def new
     @gift_suggestion = GiftSuggestion.new
   end
 
   def create
-    @gift_suggestion = GiftSuggestion.new(gift_suggestion_params)
+
+
+    count = params[:count].to_i || 3
+
+    reset = if @recipient
+              GiftSuggestion.where(event_id: @event.id, recipient_id: @recipient.id)
+            else
+              GiftSuggestion.where(event_id: @event.id, recipient_id: nil)
+            end
+
+    reset.destroy_all
+
+    count.times do |i|
+      GiftSuggestion.create!(
+        user_id:      current_user.id,
+        event_id:     @event.id,
+        recipient_id: (@recipient ? @recipient.id: nil),
+        title:        build_title(i),
+        description:  default_description(i),
+        estimated_price: suggested_price,
+        source:       "ai" #stubbing for now will connect it
+      )
+    #@gift_suggestion = GiftSuggestion.new(gift_suggestion_params)
 
     # @gift_suggestion.user = current_user
-    # @gift_suggestion.recipient =
-    # @gift_suggestion.event =
 
-    #if @gift_suggestion.save
-    flash[:notice] =  "Gift suggestion saved."
-    redirect_to new_gift_suggestion_path
 
     #else
     #  flash[:notice] = "Could not save suggestion."
     # render :new, status: :unprocessable_entity
 
+    end
+    redirect_to event_gift_suggestions_path(@event, recipient_id: @recipient.id),
+                notice: "#{count} gift suggestions generated."
 
 
   end
-
-
 
   private
-  def gift_suggestion_params
-    params.require(:gift_suggestion).permit(:title, :description)
+  # def gift_suggestion_params
+    #params.require(:gift_suggestion).permit(:title, :description)
+
+  #end
+  def set_event
+    @event = current_user.events.find(params[:event_id])
+  end
 
   end
+
+  def set_recipient
+    if params[:recipient_id].present?
+      @recipient = current_user.recipients.find_by(id: params[:recipient_id])
+    else
+      @recipient = nil
+    end
+  end
+  def build_title(i)
+    base = @event.title.presence || "Gift idea"
+    "#{base} (option #{i+1})"
+    #"#{base} + (option #{i++})"
+
+
+  end
+  def default_description(i)
+    desc = []
+    desc << "A gift idea"
+
+    if @recipient
+      desc << "for #{@recipient.name}"
+      desc << "likes #{@recipient.likes})" if @recipient.likes.present?
+      desc << "hobbies #{@recipient.hobbies})" if @recipient.hobbies.present?
+    end
+
+    desc << "for #{@event.title}" if @event.title.present?
+    desc << "with theme #{@event.theme}" if @event.theme.present?
+    desc << "(option #{i + 1})"
+
+    desc.join(" ")
+  end
+  def suggested_price
+    if @recipient && @recipient.budget.present?
+      @recipient.budget.to_f
+    else
+      100.00
+    end
+
 
 end
--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,11 +1,29 @@
 class GiftsController < ApplicationController
   def new
     @new_gift = Gift.new # for use with the form_with helper
+
+    if params[:gift_suggestion_id].present? # so if user comes from suggestions autofills
+      suggestion = GiftSuggestion.find_by(id: params[:gift_suggestion_id])
+
+      if suggestion
+        @new_gift.name         = suggestion.title
+        @new_gift.description  = suggestion.description
+        @new_gift.price        = suggestion.estimated_price
+        @new_gift.recipient_id = suggestion.recipient_id
+        @new_gift.event_id     = suggestion.event_id
+      end
+    else
+      @new_gift.recipient_id = suggestion.recipient_id
+      @new_gift.event_id     = suggestion.event_id
+
+    end
   end
 
+
   def create
-    @save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => nil, :event_id => nil}
+    #@save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => nil, :event_id => nil}
     @new_gift = Gift.new(@save_params)
+    @new_gift.user = current_user #sorry making it work with mine can change
     if @new_gift.save
       flash[:notice] = "New gift created!"
       redirect_to :root
@@ -17,6 +35,6 @@ class GiftsController < ApplicationController
 
   private
   def save_params
-    params.require(:gift).permit(:name, :description, :user_id, :recipient_id, :price, :event_id)
+    params.require(:gift).permit(:name, :description, :recipient_id, :price, :event_id)
   end
 end
