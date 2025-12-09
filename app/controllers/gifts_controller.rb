48f786ba080e0fcbbe4b41d594dc73d1e3f6b751
nicwilcox <nicwilcox@uiowa.edu>
2025-12-09 17:08:11 -0600

    Autofill saving to gift

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -11,11 +11,33 @@ class GiftsController < ApplicationController
 
   def new
     @new_gift = Gift.new # for use with the form_with helper
+
+    if params[:gift_suggestion_id].present? # so if user comes from suggestions autofills
+      suggestion = GiftSuggestion.find_by(id: params[:gift_suggestion_id])
+
+      if suggestion
+        @new_gift.name              = suggestion.title
+        @new_gift.description       = suggestion.description # need to take out "description:"???
+        @new_gift.price             = suggestion.estimated_price
+        @new_gift.recipient_id      = suggestion.recipient_id
+        @new_gift.event_id          = suggestion.event_id
+        @new_gift.best_vendor_name  = suggestion.best_vendor_name
+        @new_gift.best_vendor_url   = suggestion.best_vendor_url
+        @new_gift.best_vendor_price = suggestion.best_vendor_price
+      end
+
+    else
+      @new_gift.recipient_id ||= params[:recipient_id]
+      @new_gift.event_id ||= params[:event_id]
+
+    end
   end
 
+
   def create
-    @save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => params[:gift][:recipient_id], :event_id => nil, :visibility => params[:gift][:visibility]}
-    @new_gift = Gift.new(@save_params)
+    #@save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => params[:gift][:recipient_id], :event_id => nil, :visibility => params[:gift][:visibility]}
+    @new_gift = Gift.new(save_params.merge(user_id: current_user.id))
+
     if @new_gift.save
       flash[:notice] = "New gift created!"
       redirect_to gift_path(@new_gift.id)
@@ -31,8 +53,9 @@ class GiftsController < ApplicationController
 
   def update
     @gift = current_user.gifts.find(params[:id])
-    @save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => params[:gift][:recipient_id], :event_id => nil, :visibility => params[:gift][:visibility]}
-    if @gift.update(@save_params)
+    #@save_params = {:name => params[:gift][:name], :description => params[:gift][:description], :price => params[:gift][:price].to_f, :user_id => current_user.id, :recipient_id => params[:gift][:recipient_id], :event_id => nil, :visibility => params[:gift][:visibility]}
+    #if @gift.update(@save_params)
+    if @gift.update(save_params.merge(user_id: current_user.id))
       flash[:notice] = "Gift updated."
       redirect_to :root
     else
@@ -50,6 +73,7 @@ class GiftsController < ApplicationController
 
   private
   def save_params
-    params.require(:gift).permit(:name, :description, :user_id, :recipient_id, :price, :event_id)
+    params.require(:gift).permit(:name, :description, :user_id, :recipient_id, :price, :event_id,
+                                 :visibility, :best_vendor_name, :best_vendor_url, :best_vendor_price) #For optimal pricing prob will need status too
   end
 end
