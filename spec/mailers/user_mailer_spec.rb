0d81369874b82e544702c905edf4f6dc7c77694d
tcorme <tyler-orme@uiowa.edu>
2025-12-13 13:53:42 -0600

    test: create tests for emails
    (cherry picked from commit e59d46e4607e71ccbb44d431422fa74ef1d31e45)

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,50 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "welcome" do
+    before do
+      User.set_callback(:create, :after, :send_welcome_email)
+    end
+    it "calls the welcome action when a new user signs up" do
+      obj = double
+      other_obj = double
+      allow(other_obj).to receive(:deliver_now).and_return(nil)
+      expect(obj).to receive(:welcome).and_return(other_obj)
+      expect(UserMailer).to receive(:with).with(user:instance_of(User)).and_return(obj)
+      user1 = User.create!(email:"a@a.a", password:"password123")
+    end
+    it "sends to user with correct subject" do
+      user = double("User")
+      allow(user).to receive(:email).and_return("a@a.a")
+      mail = UserMailer.with(user:user).welcome
+      expect(mail.to).to eq(["a@a.a"])
+      expect(mail.subject).to eq("Welcome to Giftwise")
+    end
+    it "renders the view with the users email address" do
+      user = double("User")
+      allow(user).to receive(:email).and_return("a@a.a")
+      mail = UserMailer.with(user:user).welcome
+      expect(mail.body.encoded).to include(user.email)
+      expect(mail.body.encoded).to include("Welcome")
+    end
+  end
+  describe "friend_request" do
+    it "sends to user with correct subject" do
+      user = double("User")
+      requestee = double("User")
+      allow(user).to receive(:email).and_return("a@a.a")
+      allow(requestee).to receive(:email).and_return("b@b.b")
+      mail = UserMailer.with(user:user, requestee:requestee).friend_request
+      expect(mail.to).to eq(["a@a.a"])
+      expect(mail.subject).to eq("You have a new friend request")
+    end
+    it "contains the user who made the request" do
+      user = double("User")
+      requestee = double("User")
+      allow(user).to receive(:email).and_return("a@a.a")
+      allow(requestee).to receive(:email).and_return("b@b.b")
+      mail = UserMailer.with(user:user, requestee:requestee).friend_request
+      expect(mail.body.encoded).to include(requestee.email)
+    end
+  end
 end
